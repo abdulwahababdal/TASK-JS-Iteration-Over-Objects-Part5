5cb60d3f7c66218d37e75b6f90a1249bb752fa4e
Abdulwahab <abdulwahababdal@gmail.com>
2024-11-05 14:35:05 +0300

    done

--- objectIteration.js
+++ objectIteration.js
@@ -33,31 +33,45 @@ const fruits = [
 
 // 1) Write a `getFruitColor` function that accepts a `fruit` object as an argument, and returns the color of that `fruit` object
 function getFruitColor(fruit) {
-  // write your code here...
+  return fruit.color;
 }
 // console.log(getFruitColor(fruits[0])); // Outputs: Red
 
 // 2) Write a `isFruitTasteMatching` function that accepts a `fruit` object as an argument and a `taste` string, return  true if the fruit's taste matches the provided description, otherwise returns false
 function isFruitTasteMatching(fruit, taste) {
-  // write your code here...
+  return fruit.taste === taste;
 }
 // console.log(isFruitTasteMatching(fruits[2], "Citrusy")); // Outputs: true
 
 // 3) Write a `addFruit` function that accepts an array of fruit object `fruits` and the properties of a fruit (id, name, color, taste), it will add the new fruit to the end of the array, then returns the updated array
 function addFruit(fruits, id, name, color, taste) {
-  // write your code here...
+  const newFruit = {
+    id,
+    name,
+    color,
+    taste,
+  };
+  fruits.push(newFruit);
+  return fruits;
 }
 // console.log(addFruit(fruits,  506,  "Mango", "Yellow", "Sweet" ));
 
 // 4) Write a `countSweetFruits`function that accepts an array of fruit objects `fruits`, and return the number of fruits with a sweet taste
 function countSweetFruits(fruits) {
-  // write your code here...
+  return fruits.filter((fruit) => fruit.taste === "Sweet").length;
 }
 // console.log(countSweetFruits(fruits)); // Outputs: 4
 
 // 5) 🌶️🌶️🌶️ Write a function `fruitByTaste` that takes an array of fruit objects `fruits` and returns an object where the keys are tastes and the values are arrays of fruit names
 function fruitByTaste(fruits) {
-  // Write your code here...
+  return fruits.reduce((result, fruit) => {
+    const { taste, name } = fruit;
+    if (!result[taste]) {
+      result[taste] = [];
+    }
+    result[taste].push(name);
+    return result;
+  }, {});
 }
 /**
  * console.log(fruitByTaste(fruits));
